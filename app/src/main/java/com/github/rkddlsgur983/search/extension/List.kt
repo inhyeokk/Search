727357e5ecb063f86de6e70df12947d61e4c0578
rkddlsgur983 <rkddlsgur983@gmail.com>
2022-02-06 21:22:15 +0900

    feat: 파라미터 NonNull 타입으로 수정

--- app/src/main/java/com/github/rkddlsgur983/search/extension/List.kt
+++ app/src/main/java/com/github/rkddlsgur983/search/extension/List.kt
@@ -1,5 +1,5 @@
 package com.github.rkddlsgur983.search.extension
 
-fun <T> List<T>.join(itemList: List<T>?): List<T> {
-    return this.toMutableList().apply { itemList?.let { addAll(it) } }
+fun <T> List<T>.join(itemList: List<T>): List<T> {
+    return this.toMutableList().apply { addAll(itemList) }
 }
