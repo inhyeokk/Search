7f7fdbe73b932c4de0a06b2dd18ad64975a1478d
inhyeok <rkddlsgur983@gmail.com>
2022-04-12 15:16:33 +0900

    feat: searchCafe 메소드로 수정

--- app/src/main/java/com/rkddlsgur983/search/presentation/cafe/SearchCafeViewModel.kt
+++ app/src/main/java/com/rkddlsgur983/search/presentation/cafe/SearchCafeViewModel.kt
@@ -11,10 +11,7 @@ import com.rkddlsgur983.search.presentation.cafe.model.Document
 import com.rkddlsgur983.search.util.extension.fromHtml
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.flatMapLatest
-import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.*
 import javax.inject.Inject
 
 @HiltViewModel
@@ -27,9 +24,13 @@ class SearchCafeViewModel @Inject constructor(
     private val onSearchClick = MutableStateFlow(queryStateFlow.value)
 
     @ExperimentalCoroutinesApi
-    val documentListStateFlow: Flow<PagingData<Document>> = onSearchClick.flatMapLatest { query ->
-        searchCafeRepository.searchCafe(query, sortTypeStateFlow.value)
-            .cachedIn(viewModelScope).map { pagingData ->
+    val documentListStateFlow: Flow<PagingData<Document>> =
+        onSearchClick.flatMapLatest(this::searchCafe)
+
+    private fun searchCafe(query: String): Flow<PagingData<Document>> {
+        return searchCafeRepository.searchCafe(query, sortTypeStateFlow.value)
+            .cachedIn(viewModelScope) // 뷰모델 내에서 값을 캐싱 ex) activity 에서 할당한 flow 를 LiveData 처럼 fragment 에서도 참조할 수 있다.
+            .map { pagingData ->
                 pagingData.map {
                     Document(
                         id = System.currentTimeMillis(),
